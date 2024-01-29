7afec71fffd75384a404073f53bce121b7820082
spookdude <mmaa.woestenburg@student.avans.nl>
2024-01-29 11:10:01 +0100

    opdracht 1

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -5,10 +5,13 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
 public class House extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1024, 768);
+        Canvas canvas = new Canvas(500, 500);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("House");
@@ -17,7 +20,13 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
-        // test
+        graphics.draw(new Line2D.Double(100,200,250,10));
+        graphics.draw(new Line2D.Double(250,10,400,200));
+        graphics.draw(new Line2D.Double(100,200,100,400));
+        graphics.draw(new Line2D.Double(400,200,400,400));
+        graphics.draw(new Line2D.Double(100,400,400,400));
+        graphics.draw(new Rectangle2D.Double(125,300,50,100));
+        graphics.draw(new Rectangle2D.Double(225,250,150,100));
     }
 
 
