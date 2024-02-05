0a6de2b409c9c954245521b4e0aa0d97b9aaf3e0
spookdude <mmaa.woestenburg@student.avans.nl>
2024-02-05 11:22:04 +0100

    opdracht 5

--- Week1/005.Spirograph/src/Spirograph.java
+++ Week1/005.Spirograph/src/Spirograph.java
@@ -16,26 +16,29 @@ public class Spirograph extends Application {
     private TextField v2;
     private TextField v3;
     private TextField v4;
+    private Canvas canvas;
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        canvas = new Canvas(1920, 1080);
        
         VBox mainBox = new VBox();
         HBox topBar = new HBox();
         mainBox.getChildren().add(topBar);
         mainBox.getChildren().add(new Group(canvas));
         
-        topBar.getChildren().add(v1 = new TextField("300"));
+        topBar.getChildren().add(v1 = new TextField("400"));
         topBar.getChildren().add(v2 = new TextField("1"));
-        topBar.getChildren().add(v3 = new TextField("300"));
-        topBar.getChildren().add(v4 = new TextField("10"));
+        topBar.getChildren().add(v3 = new TextField("400"));
+        topBar.getChildren().add(v4 = new TextField("4"));
                 
         v1.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v2.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v3.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
         v4.textProperty().addListener(e -> draw(new FXGraphics2D(canvas.getGraphicsContext2D())));
-        
+
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(mainBox));
         primaryStage.setTitle("Spirograph");
@@ -46,6 +49,20 @@ public class Spirograph extends Application {
     public void draw(FXGraphics2D graphics) {
         //you can use Double.parseDouble(v1.getText()) to get a double value from the first textfield
         //feel free to add more textfields or other controls if needed, but beware that swing components might clash in naming
+        double a = Double.parseDouble(v1.getText());
+        double b = Double.parseDouble(v2.getText());
+        double c = Double.parseDouble(v3.getText());
+        double d = Double.parseDouble(v4.getText());
+        double x1 = a * Math.cos(b * 0) + c * Math.cos(d * 0);
+        double y1 = a * Math.sin(b * 0) + c * Math.sin(d * 0);
+        double resolution = 0.001;
+        for (double i =0;i<500;i+=resolution){
+            double x = a * Math.cos(b * i) + c * Math.cos(d * i);
+            double y = a * Math.sin(b * i) + c * Math.sin(d * i);
+            graphics.draw(new Line2D.Double(x1, y1, x, y));
+            x1 = x;
+            y1 = y;
+        }
     }
     
     
