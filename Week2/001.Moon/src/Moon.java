244b31c666581a6a5cc0a9aefabfb61e65fdc303
spookdude <mmaa.woestenburg@student.avans.nl>
2024-02-05 12:00:56 +0100

    opdracht 1

--- Week2/001.Moon/src/Moon.java
+++ Week2/001.Moon/src/Moon.java
@@ -1,12 +1,15 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
 
 public class Moon extends Application {
     private ResizableCanvas canvas;
@@ -29,6 +32,12 @@ public class Moon extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.draw(new Ellipse2D.Double(100,100,100,100));
+        graphics.setColor(Color.black);
+        graphics.fill(new Ellipse2D.Double(100,100,100,100));
+        graphics.setColor(Color.white);
+        graphics.draw(new Ellipse2D.Double(70,95,100,100));
+        graphics.fill(new Ellipse2D.Double(70,95,100,100));
     }
 
 
