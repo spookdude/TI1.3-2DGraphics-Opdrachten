db539bfb0cdae8294ef79877903d4843fc6bb5bb
spookdude <mmaa.woestenburg@student.avans.nl>
2024-02-07 15:25:53 +0100

    opdracht 3

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -7,12 +7,9 @@ import org.jfree.fx.ResizableCanvas;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
 
 public class Colors extends Application {
     private ResizableCanvas canvas;
-    private ArrayList<Color> colors = new ArrayList<>();
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -20,7 +17,6 @@ public class Colors extends Application {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
-        setColors();
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("Colors");
@@ -31,33 +27,10 @@ public class Colors extends Application {
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.black);
+        graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
-        int x = 0;
-        int y = 0;
-        for (int i=0;i<13;i++){
-            graphics.setColor(colors.get(i));
+    }
 
-            graphics.fill(new Rectangle2D.Double(x*100,y*100,100,100));
-            x +=1;
-            if (x >=6){y += 1;x=0;}
-        }
-    }
-    private void setColors(){
-        colors.add(Color.black);
-        colors.add(Color.blue);
-        colors.add(Color.cyan);
-        colors.add(Color.darkGray);
-        colors.add(Color.gray);
-        colors.add(Color.green);
-        colors.add(Color.lightGray);
-        colors.add(Color.magenta);
-        colors.add(Color.orange);
-        colors.add(Color.pink);
-        colors.add(Color.red);
-        colors.add(Color.white);
-        colors.add(Color.yellow);
-    }
 
     public static void main(String[] args)
     {
